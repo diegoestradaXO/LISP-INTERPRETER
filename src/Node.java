58c6f3e347efdf35e21cbcd537527fc1a2b29f1b
isaonaranjo <35385804+isaonaranjo@users.noreply.github.com>
2019-03-14 00:15:51 -0600

    Node class

---
+++ src/Node.java
@@ -0,0 +1,40 @@
+/**
+ * Proyecto Lisp
+ * @author Isabel Ortiz Naranjo 18176
+ * @author Diego Estrada 18540
+ * @author Michael Chan 18562
+ *
+ */
+
+public class Node <E> {
+
+    private Object Element;
+    private Node<E> Next;
+
+    // Se crea un nuevo nodo
+
+    public Node(Object element, Node<E> next) {
+        Element = element;
+        NextE = next;
+    }
+
+    // Retorna el elemento siguiente de la lista
+
+    public Node<E> next() {
+        return NextE;
+    }
+
+    // Determina el siguiente elemento
+
+    public void setNext(Node<E> next){
+        NextE = next;
+    }
+
+    // Retorna el valor asociado con el elemento
+
+    public Object value() {
+        return Element;
+    }
+
+
+}
